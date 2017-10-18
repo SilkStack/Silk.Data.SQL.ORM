c7408476f214dfaea28aab3aded7ae9c4ea86165
John Carruthers <johnc@frag-labs.com>
2017-10-18 16:44:29 -0500

    Test derp.

--- Silk.Data.SQL.ORM.Tests/InsertTests.cs
+++ Silk.Data.SQL.ORM.Tests/InsertTests.cs
@@ -31,9 +31,9 @@ namespace Silk.Data.SQL.ORM.Tests
 			};
 			dataModel.Insert(TestDb.Provider, sourceInstances);
 
-			Assert.AreNotEqual(sourceInstances[0], Guid.Empty);
-			Assert.AreNotEqual(sourceInstances[1], Guid.Empty);
-			Assert.AreNotEqual(sourceInstances[2], Guid.Empty);
+			Assert.AreNotEqual(sourceInstances[0].Id, Guid.Empty);
+			Assert.AreNotEqual(sourceInstances[1].Id, Guid.Empty);
+			Assert.AreNotEqual(sourceInstances[2].Id, Guid.Empty);
 
 			using (var queryResult = TestDb.Provider.ExecuteReader(
 				QueryExpression.Select(
@@ -59,6 +59,59 @@ namespace Silk.Data.SQL.ORM.Tests
 
 				Assert.IsTrue(queryResult.Read());
 				Assert.AreEqual(sourceInstances[2].Id, queryResult.GetGuid(0));
+				Assert.AreEqual(sourceInstances[2].Data, queryResult.GetString(1));
+			}
+		}
+
+		[TestMethod]
+		public void InsertIntSimpleModel()
+		{
+			var model = TypeModeller.GetModelOf<BasicPocoWithIntId>();
+			var dataModel = model.GetModeller<BasicPocoWithIntId>()
+				.CreateDataModel();
+
+			foreach (var table in dataModel.Tables)
+			{
+				if (!table.Exists(TestDb.Provider))
+					table.Create(TestDb.Provider);
+			}
+
+			var sourceInstances = new[]
+			{
+				new BasicPocoWithIntId { Data = "Hello World 1" },
+				new BasicPocoWithIntId { Data = "Hello World 2" },
+				new BasicPocoWithIntId { Data = "Hello World 3" }
+			};
+			dataModel.Insert(TestDb.Provider, sourceInstances);
+
+			Assert.AreNotEqual(sourceInstances[0].Id, 0);
+			Assert.AreNotEqual(sourceInstances[1].Id, 0);
+			Assert.AreNotEqual(sourceInstances[2].Id, 0);
+
+			using (var queryResult = TestDb.Provider.ExecuteReader(
+				QueryExpression.Select(
+					new[] { QueryExpression.All() },
+					from: QueryExpression.Table(dataModel.Tables.First().TableName),
+					where: QueryExpression.Compare(QueryExpression.Column("Id"), ComparisonOperator.None, QueryExpression.InFunction(sourceInstances.Select(q => (object)q.Id).ToArray())),
+					orderBy: new[] {
+						QueryExpression.Descending(QueryExpression.Compare(QueryExpression.Column("Id"), ComparisonOperator.AreEqual, QueryExpression.Value(sourceInstances[0].Id))),
+						QueryExpression.Descending(QueryExpression.Compare(QueryExpression.Column("Id"), ComparisonOperator.AreEqual, QueryExpression.Value(sourceInstances[1].Id))),
+						QueryExpression.Descending(QueryExpression.Compare(QueryExpression.Column("Id"), ComparisonOperator.AreEqual, QueryExpression.Value(sourceInstances[2].Id)))
+					}
+				)))
+			{
+				Assert.IsTrue(queryResult.HasRows);
+
+				Assert.IsTrue(queryResult.Read());
+				Assert.AreEqual(sourceInstances[0].Id, queryResult.GetInt32(0));
+				Assert.AreEqual(sourceInstances[0].Data, queryResult.GetString(1));
+
+				Assert.IsTrue(queryResult.Read());
+				Assert.AreEqual(sourceInstances[1].Id, queryResult.GetInt32(0));
+				Assert.AreEqual(sourceInstances[1].Data, queryResult.GetString(1));
+
+				Assert.IsTrue(queryResult.Read());
+				Assert.AreEqual(sourceInstances[2].Id, queryResult.GetInt32(0));
 				Assert.AreEqual(sourceInstances[2].Data, queryResult.GetString(1));
 			}
 		}
