926b97b070ea3346b9b7e55e5b5df21e02d016d7
John Carruthers <johnc@frag-labs.com>
2017-10-23 18:04:09 -0500

    Basic ExpressionConverter working.

--- Silk.Data.SQL.ORM/Expressions/ExpressionConverter.cs
+++ Silk.Data.SQL.ORM/Expressions/ExpressionConverter.cs
@@ -1,6 +1,7 @@
 ﻿using Silk.Data.SQL.Expressions;
 using Silk.Data.SQL.ORM.Modelling;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Silk.Data.SQL.ORM.Expressions
@@ -22,16 +23,118 @@ namespace Silk.Data.SQL.ORM.Expressions
 			var parameterName = expression.Parameters[0].Name;
 			_converterVisitor.Setup(parameterName);
 			_converterVisitor.Visit(expression.Body);
-			return null;
+			return _converterVisitor.PopFromStack();
 		}
 
 		private class ConverterVisitor : ExpressionVisitor
 		{
 			private string _parameterName;
+			private readonly Stack<QueryExpression> _expressionStack = new Stack<QueryExpression>();
 
 			public void Setup(string parameterName)
 			{
 				_parameterName = parameterName;
+				_expressionStack.Clear();
+			}
+
+			private QueryExpression ConvertExpression(Expression expression)
+			{
+				var stackSize = _expressionStack.Count;
+				Visit(expression);
+				if (stackSize == _expressionStack.Count)
+					throw new UnsupportedExpressionException(expression);
+				return PopFromStack();
+			}
+
+			public QueryExpression PopFromStack()
+			{
+				return _expressionStack.Pop();
+			}
+
+			private void PushOntoStack(QueryExpression queryExpression)
+			{
+				_expressionStack.Push(queryExpression);
+			}
+
+			protected override Expression VisitConstant(ConstantExpression node)
+			{
+				PushOntoStack(QueryExpression.Value(node.Value));
+				return node;
+			}
+
+			protected override Expression VisitMember(MemberExpression node)
+			{
+				var expressionParameter = GetParameterExpression(node);
+				if (expressionParameter != null)
+				{
+					//  member is based from a parameter to a lambda expression
+					PushOntoStack(QueryExpression.Column(node.Member.Name));
+				}
+				else
+				{
+					//  member is based from a hoisted parameter
+					var memberAccessExp = Expression.MakeMemberAccess(node.Expression, node.Member);
+					var @delegate = Expression.Lambda<Func<object>>(Expression.Convert(memberAccessExp, typeof(object)));
+					var value = @delegate.Compile()();
+					PushOntoStack(QueryExpression.Value(value));
+				}
+				return node;
+			}
+
+			private static ParameterExpression GetParameterExpression(MemberExpression memberExpression)
+			{
+				if (memberExpression.Expression == null)
+					return null;
+				if (memberExpression.Expression is ParameterExpression parameterExpr)
+					return parameterExpr;
+				if (memberExpression.Expression is MemberExpression subMemberExpression)
+					return GetParameterExpression(subMemberExpression);
+				return null;
+			}
+
+			protected override Expression VisitBinary(BinaryExpression node)
+			{
+				var left = ConvertExpression(node.Left);
+				var right = ConvertExpression(node.Right);
+				switch (node.NodeType)
+				{
+					case ExpressionType.AndAlso:
+						PushOntoStack(
+							QueryExpression.AndAlso(left, right)
+						);
+						break;
+					case ExpressionType.OrElse:
+						PushOntoStack(
+							QueryExpression.OrElse(left, right)
+						);
+						break;
+					case ExpressionType.Equal:
+					case ExpressionType.NotEqual:
+					case ExpressionType.GreaterThan:
+					case ExpressionType.GreaterThanOrEqual:
+					case ExpressionType.LessThan:
+					case ExpressionType.LessThanOrEqual:
+						PushOntoStack(
+							QueryExpression.Compare(left, GetComparisonOperator(node.NodeType), right)
+						);
+						break;
+
+				}
+				return node;
+			}
+
+			private static ComparisonOperator GetComparisonOperator(ExpressionType linqExpressionType)
+			{
+				switch (linqExpressionType)
+				{
+					case ExpressionType.Equal: return ComparisonOperator.AreEqual;
+					case ExpressionType.NotEqual: return ComparisonOperator.AreNotEqual;
+					case ExpressionType.GreaterThan: return ComparisonOperator.GreaterThan;
+					case ExpressionType.GreaterThanOrEqual: return ComparisonOperator.GreaterThanOrEqualTo;
+					case ExpressionType.LessThan: return ComparisonOperator.LessThan;
+					case ExpressionType.LessThanOrEqual: return ComparisonOperator.LessThanOrEqualTo;
+				}
+				throw new ArgumentException("Unknown expression type.", nameof(linqExpressionType));
 			}
 		}
 	}
---
+++ Silk.Data.SQL.ORM/Expressions/UnsupportedExpressionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Silk.Data.SQL.ORM.Expressions
+{
+	public class UnsupportedExpressionException : Exception
+	{
+		public UnsupportedExpressionException(Expression expression) : base("Unsupported expression.")
+		{
+			Expression = expression;
+		}
+
+		public Expression Expression { get; }
+	}
+}
