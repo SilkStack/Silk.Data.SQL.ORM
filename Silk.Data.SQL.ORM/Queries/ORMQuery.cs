ab476c85b8df6c1433e781a21a7e06f24658e85e
John Carruthers <johnc@frag-labs.com>
2017-11-26 22:40:11 -0600

    Insert objects with auto increment IDs.

--- Silk.Data.SQL.ORM/Queries/ORMQuery.cs
+++ Silk.Data.SQL.ORM/Queries/ORMQuery.cs
@@ -1,4 +1,6 @@
-﻿using Silk.Data.SQL.Expressions;
+﻿using Silk.Data.Modelling;
+using Silk.Data.SQL.Expressions;
+using Silk.Data.SQL.ORM.Modelling;
 using Silk.Data.SQL.Queries;
 using System;
 using System.Threading.Tasks;
@@ -34,6 +36,52 @@ namespace Silk.Data.SQL.ORM.Queries
 	{
 		public NoResultORMQuery(QueryExpression query) : base(query)
 		{
+		}
+	}
+
+	public class AssignAutoIncrementORMQuery : ORMQuery
+	{
+		private readonly DataField _field;
+		private readonly IContainerReadWriter _containerReadWriter;
+
+		public AssignAutoIncrementORMQuery(QueryExpression query, Type mapToType,
+			DataField field, IContainerReadWriter containerReadWriter)
+			: base(query, mapToType)
+		{
+			_field = field;
+			_containerReadWriter = containerReadWriter;
+		}
+
+		public override object MapResult(QueryResult queryResult)
+		{
+			if (queryResult.Read())
+			{
+				if (MapToType == typeof(short))
+					_field.ModelBinding.WriteValue(_containerReadWriter, queryResult.GetInt16(0));
+				else if (MapToType == typeof(long))
+					_field.ModelBinding.WriteValue(_containerReadWriter, queryResult.GetInt64(0));
+				else if (MapToType == typeof(int))
+					_field.ModelBinding.WriteValue(_containerReadWriter, queryResult.GetInt32(0));
+			}
+			else
+				throw new Exception("Failed to get auto generated ID.");
+			return null;
+		}
+
+		public override async Task<object> MapResultAsync(QueryResult queryResult)
+		{
+			if (await queryResult.ReadAsync().ConfigureAwait(false))
+			{
+				if (MapToType == typeof(short))
+					_field.ModelBinding.WriteValue(_containerReadWriter, queryResult.GetInt16(0));
+				else if (MapToType == typeof(long))
+					_field.ModelBinding.WriteValue(_containerReadWriter, queryResult.GetInt64(0));
+				else if (MapToType == typeof(int))
+					_field.ModelBinding.WriteValue(_containerReadWriter, queryResult.GetInt32(0));
+			}
+			else
+				throw new Exception("Failed to get auto generated ID.");
+			return null;
 		}
 	}
 }
