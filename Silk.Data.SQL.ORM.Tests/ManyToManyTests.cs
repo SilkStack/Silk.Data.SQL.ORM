9b25dde0a76e8ede17efb742c4941a9d20d6bae5
John Carruthers <johnc@frag-labs.com>
2017-12-21 14:23:25 -0600

    Test many to many modelling.

--- Silk.Data.SQL.ORM.Tests/ManyToManyTests.cs
+++ Silk.Data.SQL.ORM.Tests/ManyToManyTests.cs
@@ -2,6 +2,7 @@
 using Silk.Data.SQL.ORM.Modelling;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Silk.Data.SQL.ORM.Tests
 {
@@ -21,7 +22,45 @@ namespace Silk.Data.SQL.ORM.Tests
 			Assert.AreEqual(3, model.Fields.Length);
 			Assert.AreEqual(3, model.Schema.Tables.Count);
 
-			Assert.Fail("Test not complete");
+			var fieldForRelationshipA = model.Fields.FirstOrDefault(q => q.Name == "RelationshipA");
+			var fieldForRelationshipB = model.Fields.FirstOrDefault(q => q.Name == "RelationshipB");
+
+			Assert.IsNotNull(fieldForRelationshipA);
+			Assert.IsNotNull(fieldForRelationshipA.Relationship);
+			Assert.ReferenceEquals(model.Domain.GetEntityModel<RelationshipTypeA>(), fieldForRelationshipA.Relationship.ForeignModel);
+			Assert.AreEqual(RelationshipType.ManyToMany, fieldForRelationshipA.Relationship.RelationshipType);
+			Assert.IsNull(fieldForRelationshipA.Storage);
+
+			Assert.IsNotNull(fieldForRelationshipB);
+			Assert.IsNotNull(fieldForRelationshipB.Relationship);
+			Assert.ReferenceEquals(model.Domain.GetEntityModel<RelationshipTypeB>(), fieldForRelationshipB.Relationship.ForeignModel);
+			Assert.AreEqual(RelationshipType.ManyToMany, fieldForRelationshipB.Relationship.RelationshipType);
+			Assert.IsNull(fieldForRelationshipB.Storage);
+
+			Assert.AreEqual(1, model.Schema.EntityTable.DataFields.Length);
+			var idField = model.Schema.EntityTable.DataFields.FirstOrDefault(q => q.Name == "Id");
+			Assert.IsNotNull(idField);
+			Assert.AreEqual(typeof(Guid), idField.DataType);
+			Assert.IsTrue(idField.Storage.IsAutoGenerate);
+			Assert.IsTrue(idField.Storage.IsPrimaryKey);
+
+			var relationshipATable = model.Schema.RelationshipTables.FirstOrDefault(q => q.TableName == "PocoWithManyRelationshipsToRelationshipTypeA");
+			Assert.AreEqual(2, relationshipATable.DataFields.Length);
+			var modelIdField = relationshipATable.DataFields.FirstOrDefault(q => q.Name == "PocoWithManyRelationships_Id");
+			Assert.IsNotNull(modelIdField);
+			Assert.AreEqual(typeof(Guid), modelIdField.DataType);
+			var relationshipIdField = relationshipATable.DataFields.FirstOrDefault(q => q.Name == "RelationshipTypeA_Id");
+			Assert.IsNotNull(relationshipIdField);
+			Assert.AreEqual(typeof(int), relationshipIdField.DataType);
+
+			var relationshipBTable = model.Schema.RelationshipTables.FirstOrDefault(q => q.TableName == "PocoWithManyRelationshipsToRelationshipTypeB");
+			Assert.AreEqual(2, relationshipBTable.DataFields.Length);
+			modelIdField = relationshipBTable.DataFields.FirstOrDefault(q => q.Name == "PocoWithManyRelationships_Id");
+			Assert.IsNotNull(modelIdField);
+			Assert.AreEqual(typeof(Guid), modelIdField.DataType);
+			relationshipIdField = relationshipBTable.DataFields.FirstOrDefault(q => q.Name == "RelationshipTypeB_Id");
+			Assert.IsNotNull(relationshipIdField);
+			Assert.AreEqual(typeof(Guid), relationshipIdField.DataType);
 		}
 
 		private class PocoWithManyRelationships
