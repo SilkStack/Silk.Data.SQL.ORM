d709fa0da7c5644b95cd79902dd97cb0566c78a3
John Carruthers <johnc@frag-labs.com>
2018-01-04 20:20:30 -0600

    Remove model driven one-to-many modelling until a design is decided upon.

--- Silk.Data.SQL.ORM/Modelling/Conventions/ManyToOneConvention.cs
+++ Silk.Data.SQL.ORM/Modelling/Conventions/ManyToOneConvention.cs
@@ -6,63 +6,13 @@ namespace Silk.Data.SQL.ORM.Modelling.Conventions
 {
 	public class ManyToOneConvention : ViewConvention<DataViewBuilder>
 	{
-		public override ViewType SupportedViewTypes => ViewType.All;
+		public override ViewType SupportedViewTypes => ViewType.ConventionDerived;
 		public override bool PerformMultiplePasses => true;
 		public override bool SkipIfFieldDefined => false;
 
 		public override void MakeModelField(DataViewBuilder viewBuilder, ModelField field)
 		{
-			if (viewBuilder.Mode == ViewType.ConventionDerived)
-				MakeConventionDrivenFields(viewBuilder, field);
-			else if (viewBuilder.Mode == ViewType.ModelDriven)
-				MakeModelDrivenFields(viewBuilder, field);
-		}
-
-		public void MakeModelDrivenFields(DataViewBuilder viewBuilder, ModelField field)
-		{
-			var possiblePaths = ConventionHelpers.GetPaths(field.Name);
-			if (possiblePaths == null)
-				return;
-
-			foreach (var candidatePath in possiblePaths)
-			{
-				var sourceField = viewBuilder.FindSourceField(field, candidatePath, dataType: field.DataType);
-				if (sourceField == null)
-					continue;
-
-				var rootSourceField = viewBuilder.FindSourceField(field, candidatePath[0]);
-				if (viewBuilder.IsPrimitiveType(rootSourceField.Field.DataType) || rootSourceField.Field.IsEnumerable)
-					continue;
-
-				var dataTypeSchemaDefinition = viewBuilder.GetSchemaDefinitionFor(rootSourceField.Field.DataType);
-				if (dataTypeSchemaDefinition == null)
-					continue;
-
-				var dataTypeEntityTable = dataTypeSchemaDefinition.TableDefinitions.FirstOrDefault(q => q.IsEntityTable);
-				if (dataTypeEntityTable == null)
-					continue;
-
-				var primaryKeyField = dataTypeEntityTable.Fields.FirstOrDefault(q => q.Name == candidatePath.Last() &&  q.Metadata.OfType<PrimaryKeyAttribute>().Any());
-				if (primaryKeyField == null)
-					continue;
-
-				var fieldName = string.Join("", candidatePath);
-
-				var existingField = viewBuilder.GetDefinedField(fieldName);
-				if (existingField != null)
-				{
-					viewBuilder.UndefineField(existingField);
-				}
-
-				viewBuilder.DefineManyToOneViewField(sourceField, candidatePath, fieldName,
-					new RelationshipDefinition
-					{
-						EntityType = rootSourceField.Field.DataType,
-						RelationshipField = primaryKeyField.Name,
-						RelationshipType = RelationshipType.ManyToOne
-					},
-					new IsNullableAttribute(true));
-			}
+			MakeConventionDrivenFields(viewBuilder, field);
 		}
 
 		public void MakeConventionDrivenFields(DataViewBuilder viewBuilder, ModelField field)
