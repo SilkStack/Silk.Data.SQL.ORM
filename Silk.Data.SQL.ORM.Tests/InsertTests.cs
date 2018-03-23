c545d02dcd161c1db24850ec73c9894d82fac620
John Carruthers <johnc@frag-labs.com>
2018-03-23 17:07:24 -0500

    Start INSERT support.

---
+++ Silk.Data.SQL.ORM.Tests/InsertTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Silk.Data.SQL.Expressions;
+using Silk.Data.SQL.ORM.Operations;
+using Silk.Data.SQL.ORM.Schema;
+
+namespace Silk.Data.SQL.ORM.Tests
+{
+	[TestClass]
+	public class InsertTests
+	{
+		[TestMethod]
+		public void GenerateSimpleInsertSQL()
+		{
+			var schemaBuilder = new SchemaBuilder();
+			schemaBuilder.DefineEntity<SimplePoco>();
+			var schema = schemaBuilder.Build();
+			var model = schema.GetEntityModel<SimplePoco>();
+
+			var data = new SimplePoco[]
+			{
+				new SimplePoco { Data = "Hello" },
+				new SimplePoco { Data = "World" }
+			};
+
+			var insert = InsertOperation.Create<SimplePoco>(model, data);
+			var insertExpression = insert.GetQuery() as InsertExpression;
+			Assert.IsNotNull(insertExpression);
+			var query = new TestQueryConverter().ConvertToQuery(insertExpression);
+			var sql = TestQueryConverter.CleanSql(query.SqlText);
+			Assert.AreEqual(@"INSERT INTO [SimplePoco] ([Data]) VALUES ( @valueParameter1 ) , ( @valueParameter2 ) ;", sql);
+			Assert.AreEqual(2, query.QueryParameters.Count);
+			Assert.IsTrue(query.QueryParameters.ContainsKey("valueParameter1"));
+			Assert.IsTrue(query.QueryParameters.ContainsKey("valueParameter2"));
+			Assert.AreEqual(data[0].Data, query.QueryParameters["valueParameter1"].Value);
+			Assert.AreEqual(data[1].Data, query.QueryParameters["valueParameter2"].Value);
+		}
+
+		private class SimplePoco
+		{
+			public string Data { get; set; }
+		}
+	}
+}
--- Silk.Data.SQL.ORM/Operations/InsertOperation.cs
+++ Silk.Data.SQL.ORM/Operations/InsertOperation.cs
@@ -1,37 +1,162 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Silk.Data.Modelling;
 using Silk.Data.SQL.Expressions;
 using Silk.Data.SQL.ORM.Modelling;
+using Silk.Data.SQL.ORM.Operations.Expressions;
 using Silk.Data.SQL.Queries;
 
 namespace Silk.Data.SQL.ORM.Operations
 {
 	public class InsertOperation : DataOperation
 	{
-		public override bool CanBeBatched => throw new System.NotImplementedException();
+		public bool GeneratesValuesServerSide { get; private set; }
 
-		public static InsertOperation Create<TEntity>(EntityModel<TEntity> model, params TEntity[] entities)
-		{
-			return null;
-		}
+		private readonly QueryExpression _bulkInsertExpression;
+		private readonly List<QueryExpression> _individualInsertExpressions;
 
-		public static InsertOperation Create<TEntity, TProjection>(EntityModel<TEntity> model, params TProjection[] projections)
+		public override bool CanBeBatched => !GeneratesValuesServerSide;
+
+		public InsertOperation(QueryExpression bulkInsertExpression,
+			List<QueryExpression> individualInsertExpressions)
 		{
-			return null;
+			GeneratesValuesServerSide = individualInsertExpressions.Count > 0;
+			_bulkInsertExpression = bulkInsertExpression;
+			_individualInsertExpressions = individualInsertExpressions;
 		}
 
 		public override QueryExpression GetQuery()
 		{
-			throw new System.NotImplementedException();
+			if (_individualInsertExpressions == null || _individualInsertExpressions.Count < 1)
+				return _bulkInsertExpression;
+
+			if (_bulkInsertExpression != null)
+				return new CompositeQueryExpression(
+					_individualInsertExpressions.Concat(new[] { _bulkInsertExpression }).ToArray()
+					);
+			return new CompositeQueryExpression(
+				_individualInsertExpressions.ToArray()
+				);
 		}
 
 		public override void ProcessResult(QueryResult queryResult)
 		{
-			throw new System.NotImplementedException();
+			if (_individualInsertExpressions == null || _individualInsertExpressions.Count < 1)
+				return;
+
+			foreach (var individualInsert in _individualInsertExpressions)
+			{
+				queryResult.NextResult();
+			}
 		}
 
-		public override Task ProcessResultAsync(QueryResult queryResult)
+		public override async Task ProcessResultAsync(QueryResult queryResult)
 		{
-			throw new System.NotImplementedException();
+			if (_individualInsertExpressions == null || _individualInsertExpressions.Count < 1)
+				return;
+
+			foreach (var individualInsert in _individualInsertExpressions)
+			{
+				await queryResult.NextResultAsync();
+			}
+		}
+
+		public static InsertOperation Create<TEntity>(EntityModel<TEntity> model, params TEntity[] entities)
+		{
+			return Create<TEntity>(model, model, entities);
+		}
+
+		public static InsertOperation Create<TEntity, TProjection>(EntityModel<TEntity> model, params TProjection[] projections)
+		{
+			return Create<TProjection>(model.GetProjection<TProjection>(), model, projections);
+		}
+
+		private static InsertOperation Create<TProjection>(ProjectionModel projectionModel, EntityModel entityModel, params TProjection[] projections)
+		{
+			List<QueryExpression> individualInsertExpressions = new List<QueryExpression>();
+
+			var primaryKeyField = entityModel.Fields.OfType<IValueField>().FirstOrDefault(q => q.Column.IsPrimaryKey);
+			var primaryKeyIsOnProjection = false;
+			if (primaryKeyField != null)
+			{
+				primaryKeyIsOnProjection = projectionModel.Fields.OfType<IValueField>().Any(q => q.Column.ColumnName == primaryKeyField.Column.ColumnName);
+			}
+
+			var transformer = new ColumnHelperTransformer();
+			var columns = new List<ColumnHelper>();
+			foreach (var projectionField in projectionModel.Fields)
+			{
+				if (projectionField is IValueField valueField)
+				{
+					valueField.Transform(transformer);
+					columns.Add(transformer.Current);
+				}
+			}
+
+			var bulkInsertRows = new List<QueryExpression[]>();
+			foreach (var obj in projections)
+			{
+				var readWriter = new ObjectReadWriter(obj, projectionModel, typeof(TProjection));
+				var row = new QueryExpression[columns.Count];
+				var i = 0;
+				foreach (var column in columns)
+				{
+					row[i] = column.GetColumnExpression(readWriter);
+				}
+				bulkInsertRows.Add(row);
+			}
+
+			return new InsertOperation(
+				bulkInsertRows.Count > 0 ? QueryExpression.Insert(entityModel.EntityTable.TableName, columns.Select(q => q.ColumnName).ToArray(), bulkInsertRows.ToArray()) : null,
+				individualInsertExpressions);
+		}
+
+		private abstract class ColumnHelper
+		{
+			public string ColumnName { get; }
+
+			public ColumnHelper(string columnName)
+			{
+				ColumnName = columnName;
+			}
+
+			public abstract QueryExpression GetColumnExpression(IModelReadWriter modelReadWriter);
+		}
+
+		private class ColumnValueReader<T> : ColumnHelper
+		{
+			private readonly string _fieldName;
+
+			public ColumnValueReader(string columnName, string fieldName) : base(columnName)
+			{
+				_fieldName = fieldName;
+			}
+
+			public override QueryExpression GetColumnExpression(IModelReadWriter modelReadWriter)
+			{
+				return QueryExpression.Value(
+					modelReadWriter.ReadField<T>(new[] { _fieldName }, 0)
+					);
+			}
+		}
+
+		private class ColumnHelperTransformer : IModelTransformer
+		{
+			public ColumnHelper Current { get; private set; }
+
+			public void VisitField<T>(IField<T> field)
+			{
+				if (field is IValueField valueField)
+				{
+					Current = new ColumnValueReader<T>(valueField.Column.ColumnName, valueField.FieldName);
+				}
+			}
+
+			public void VisitModel<TField>(IModel<TField> model) where TField : IField
+			{
+				throw new System.NotImplementedException();
+			}
 		}
 	}
 }
