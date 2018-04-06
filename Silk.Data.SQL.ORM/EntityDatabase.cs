0fca2256db2d46254e777428ac8581915ff0f165
John Carruthers <johnc@frag-labs.com>
2018-04-06 14:52:01 -0500

    Start the simply IDatabase<T> API.

---
+++ Silk.Data.SQL.ORM/EntityDatabase.cs
@@ -0,0 +1,86 @@
+﻿using Silk.Data.SQL.ORM.Modelling;
+using Silk.Data.SQL.ORM.Operations;
+using Silk.Data.SQL.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Silk.Data.SQL.ORM
+{
+	public class EntityDatabase<T> : IDatabase<T>
+		where T : class
+	{
+		private readonly EntityModel<T> _entityModel;
+
+		public Schema.Schema DataSchema { get; }
+		public IDataProvider DataProvider { get; }
+
+		public EntityDatabase(Schema.Schema schema, IDataProvider dataProvider)
+		{
+			DataProvider = dataProvider;
+			DataSchema = schema;
+
+			_entityModel = DataSchema.GetEntityModel<T>();
+			if (_entityModel == null)
+				throw new InvalidOperationException($"Knowledge of entity type {typeof(T).FullName} not present in provided schema.");
+		}
+
+		private InsertOperation CreateInsertOperation(IEnumerable<T> entities)
+		{
+			return InsertOperation.Create<T>(_entityModel, entities.ToArray());
+		}
+
+		private InsertOperation CreateInsertOperation<TView>(IEnumerable<TView> entities)
+			where TView : class
+		{
+			return InsertOperation.Create<T, TView>(_entityModel, entities.ToArray());
+		}
+
+		private void ExecuteInsertOperation(InsertOperation operation)
+		{
+			if (!operation.GeneratesValuesServerSide)
+			{
+				DataProvider.ExecuteNonQuery(operation.GetQuery());
+				return;
+			}
+
+			using (var queryResult = DataProvider.ExecuteReader(operation.GetQuery()))
+				operation.ProcessResult(queryResult);
+		}
+
+		private async Task ExecuteInsertOperationAsync(InsertOperation operation)
+		{
+			if (!operation.GeneratesValuesServerSide)
+			{
+				await DataProvider.ExecuteNonQueryAsync(operation.GetQuery());
+				return;
+			}
+
+			using (var queryResult = await DataProvider.ExecuteReaderAsync(operation.GetQuery()))
+				await operation.ProcessResultAsync(queryResult);
+		}
+
+		public void Insert(IEnumerable<T> entities)
+		{
+			ExecuteInsertOperation(CreateInsertOperation(entities));
+		}
+
+		public void Insert<TView>(IEnumerable<TView> entities)
+			where TView : class
+		{
+			ExecuteInsertOperation(CreateInsertOperation(entities));
+		}
+
+		public Task InsertAsync(IEnumerable<T> entities)
+		{
+			return ExecuteInsertOperationAsync(CreateInsertOperation(entities));
+		}
+
+		public Task InsertAsync<TView>(IEnumerable<TView> entities)
+			where TView : class
+		{
+			return ExecuteInsertOperationAsync(CreateInsertOperation(entities));
+		}
+	}
+}
---
+++ Silk.Data.SQL.ORM/IDatabase.cs
@@ -0,0 +1,27 @@
+﻿using Silk.Data.SQL.Providers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Silk.Data.SQL.ORM
+{
+	/// <summary>
+	/// Database of <see cref="T"/>.
+	/// </summary>
+	/// <typeparam name="T">Entity type to store in the database.</typeparam>
+	public interface IDatabase<T> where T : class
+	{
+		/// <summary>
+		/// Gets the schema that contains knowledge of the entity type.
+		/// </summary>
+		Schema.Schema DataSchema { get; }
+		/// <summary>
+		/// Gets the data provider used to execute queries.
+		/// </summary>
+		IDataProvider DataProvider { get; }
+
+		void Insert(IEnumerable<T> entities);
+		void Insert<TView>(IEnumerable<TView> entities) where TView : class;
+		Task InsertAsync(IEnumerable<T> entities);
+		Task InsertAsync<TView>(IEnumerable<TView> entities) where TView : class;
+	}
+}
