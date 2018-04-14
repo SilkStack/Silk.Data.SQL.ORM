3e594b99c53628ca6ae58fdc42466f603a3774a5
John Carruthers <johnc@frag-labs.com>
2018-04-13 19:45:25 -0500

    Refactor to execute queries on IQueryProvider instead of IDataProvider.

--- Silk.Data.SQL.ORM/ExecutionExtensions.cs
+++ Silk.Data.SQL.ORM/ExecutionExtensions.cs
@@ -7,7 +7,7 @@ namespace Silk.Data.SQL.ORM
 {
 	public static class ExecutionExtensions
 	{
-		public static void ExecuteBulk(this IDataProvider dataProvider, BulkOperation bulkOperation)
+		public static void ExecuteBulk(this Providers.IQueryProvider dataProvider, BulkOperation bulkOperation)
 		{
 			foreach (var operation in bulkOperation.GetOperations())
 			{
@@ -18,7 +18,7 @@ namespace Silk.Data.SQL.ORM
 			}
 		}
 
-		public static async Task ExecuteBulkAsync(this IDataProvider dataProvider, BulkOperation bulkOperation)
+		public static async Task ExecuteBulkAsync(this Providers.IQueryProvider dataProvider, BulkOperation bulkOperation)
 		{
 			foreach (var operation in bulkOperation.GetOperations())
 			{
@@ -29,14 +29,14 @@ namespace Silk.Data.SQL.ORM
 			}
 		}
 
-		public static void ExecuteNonReader(this IDataProvider dataProvider, DataOperation operation)
+		public static void ExecuteNonReader(this Providers.IQueryProvider dataProvider, DataOperation operation)
 		{
 			var query = operation.GetQuery();
 			if (query != null)
 				dataProvider.ExecuteNonQuery(query);
 		}
 
-		public static Task ExecuteNonReaderAsync(this IDataProvider dataProvider, DataOperation operation)
+		public static Task ExecuteNonReaderAsync(this Providers.IQueryProvider dataProvider, DataOperation operation)
 		{
 			var query = operation.GetQuery();
 			if (query != null)
@@ -44,41 +44,41 @@ namespace Silk.Data.SQL.ORM
 			return Task.CompletedTask;
 		}
 
-		public static void ExecuteReader<T>(this IDataProvider dataProvider, DataOperationWithResult<T> operation)
+		public static void ExecuteReader<T>(this Providers.IQueryProvider dataProvider, DataOperationWithResult<T> operation)
 		{
 			using (var result = dataProvider.ExecuteReader(operation.GetQuery()))
 				operation.ProcessResult(result);
 		}
 
-		public static async Task ExecuteReaderAsync<T>(this IDataProvider dataProvider, DataOperationWithResult<T> operation)
+		public static async Task ExecuteReaderAsync<T>(this Providers.IQueryProvider dataProvider, DataOperationWithResult<T> operation)
 		{
 			using (var result = await dataProvider.ExecuteReaderAsync(operation.GetQuery()))
 				await operation.ProcessResultAsync(result);
 		}
 
-		public static T Get<T>(this IDataProvider dataProvider, DataOperationWithResult<T> operation)
+		public static T Get<T>(this Providers.IQueryProvider dataProvider, DataOperationWithResult<T> operation)
 		{
 			dataProvider.ExecuteReader(operation);
 			return operation.Result;
 		}
 
-		public static async Task<T> GetAsync<T>(this IDataProvider dataProvider, DataOperationWithResult<T> operation)
+		public static async Task<T> GetAsync<T>(this Providers.IQueryProvider dataProvider, DataOperationWithResult<T> operation)
 		{
 			await dataProvider.ExecuteReaderAsync(operation);
 			return operation.Result;
 		}
 
-		public static T GetSingle<T>(this IDataProvider dataProvider, SelectOperation<T> select)
+		public static T GetSingle<T>(this Providers.IQueryProvider dataProvider, SelectOperation<T> select)
 		{
 			return dataProvider.Get(select).FirstOrDefault();
 		}
 
-		public static async Task<T> GetSingleAsync<T>(this IDataProvider dataProvider, SelectOperation<T> select)
+		public static async Task<T> GetSingleAsync<T>(this Providers.IQueryProvider dataProvider, SelectOperation<T> select)
 		{
 			return (await dataProvider.GetAsync(select)).FirstOrDefault();
 		}
 
-		public static void Insert(this IDataProvider dataProvider, InsertOperation insert)
+		public static void Insert(this Providers.IQueryProvider dataProvider, InsertOperation insert)
 		{
 			if (!insert.GeneratesValuesServerSide)
 			{
@@ -90,7 +90,7 @@ namespace Silk.Data.SQL.ORM
 				insert.ProcessResult(queryResult);
 		}
 
-		public static async Task InsertAsync(this IDataProvider dataProvider, InsertOperation insert)
+		public static async Task InsertAsync(this Providers.IQueryProvider dataProvider, InsertOperation insert)
 		{
 			if (!insert.GeneratesValuesServerSide)
 			{
