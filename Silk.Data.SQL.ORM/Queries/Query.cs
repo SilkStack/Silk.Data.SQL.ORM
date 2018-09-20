489bf2eeb3567b2b2bdbd22157fe60333ee728b5
John Carruthers <johnc@frag-labs.com>
2018-09-20 00:19:20 -0500

    Add query extensions for working with the result of a query without having to keep a reference to the query itself.

--- Silk.Data.SQL.ORM/Queries/Query.cs
+++ Silk.Data.SQL.ORM/Queries/Query.cs
@@ -1,6 +1,8 @@
 ﻿using Silk.Data.SQL.Expressions;
 using Silk.Data.SQL.Queries;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Silk.Data.SQL.ORM.Queries
@@ -137,6 +139,170 @@ namespace Silk.Data.SQL.ORM.Queries
 		public override async Task ProcessResultAsync(QueryResult queryResult)
 		{
 			Result = await _resultMapper.MapAllAsync(queryResult);
+		}
+	}
+
+	public class WithMappedResultQuery<T> : Query
+		where T : class
+	{
+		private readonly QueryWithMappedResult<T> _query;
+		private readonly Action<ICollection<T>> _callback;
+
+		public WithMappedResultQuery(QueryWithMappedResult<T> query, Action<ICollection<T>> callback)
+			: base(query.QueryExpression, query.ProducesResultSet)
+		{
+			_query = query;
+			_callback = callback;
+		}
+
+		public override void ProcessResult(QueryResult queryResult)
+		{
+			_query.ProcessResult(queryResult);
+			_callback?.Invoke(_query.Result);
+		}
+
+		public override async Task ProcessResultAsync(QueryResult queryResult)
+		{
+			await _query.ProcessResultAsync(queryResult);
+			_callback?.Invoke(_query.Result);
+		}
+	}
+
+	public class WithScalarResultQuery<T> : Query
+		where T : struct
+	{
+		private readonly QueryWithScalarResult<T> _query;
+		private readonly Action<ICollection<T>> _callback;
+
+		public WithScalarResultQuery(QueryWithScalarResult<T> query, Action<ICollection<T>> callback)
+			: base(query.QueryExpression, query.ProducesResultSet)
+		{
+			_query = query;
+			_callback = callback;
+		}
+
+		public override void ProcessResult(QueryResult queryResult)
+		{
+			_query.ProcessResult(queryResult);
+			_callback?.Invoke(_query.Result);
+		}
+
+		public override async Task ProcessResultAsync(QueryResult queryResult)
+		{
+			await _query.ProcessResultAsync(queryResult);
+			_callback?.Invoke(_query.Result);
+		}
+	}
+
+	public class WithTupleResultQuery<T1, T2> : Query
+		where T1 : class
+		where T2 : class
+	{
+		private readonly QueryWithTupleResult<T1, T2> _query;
+		private readonly Action<ICollection<(T1, T2)>> _callback;
+
+		public WithTupleResultQuery(QueryWithTupleResult<T1, T2> query, Action<ICollection<(T1, T2)>> callback)
+			: base(query.QueryExpression, query.ProducesResultSet)
+		{
+			_query = query;
+			_callback = callback;
+		}
+
+		public override void ProcessResult(QueryResult queryResult)
+		{
+			_query.ProcessResult(queryResult);
+			_callback?.Invoke(_query.Result);
+		}
+
+		public override async Task ProcessResultAsync(QueryResult queryResult)
+		{
+			await _query.ProcessResultAsync(queryResult);
+			_callback?.Invoke(_query.Result);
+		}
+	}
+
+	public class WithFirstMappedResultQuery<T> : Query
+		where T : class
+	{
+		private readonly QueryWithMappedResult<T> _query;
+		private readonly Action<T> _callback;
+
+		public WithFirstMappedResultQuery(QueryWithMappedResult<T> query, Action<T> callback)
+			: base(query.QueryExpression, query.ProducesResultSet)
+		{
+			_query = query;
+			_callback = callback;
+		}
+
+		public override void ProcessResult(QueryResult queryResult)
+		{
+			_query.ProcessResult(queryResult);
+			if (_query.Result.Count > 0)
+				_callback?.Invoke(_query.Result.First());
+		}
+
+		public override async Task ProcessResultAsync(QueryResult queryResult)
+		{
+			await _query.ProcessResultAsync(queryResult);
+			if (_query.Result.Count > 0)
+				_callback?.Invoke(_query.Result.First());
+		}
+	}
+
+	public class WithFirstScalarResultQuery<T> : Query
+		where T : struct
+	{
+		private readonly QueryWithScalarResult<T> _query;
+		private readonly Action<T> _callback;
+
+		public WithFirstScalarResultQuery(QueryWithScalarResult<T> query, Action<T> callback)
+			: base(query.QueryExpression, query.ProducesResultSet)
+		{
+			_query = query;
+			_callback = callback;
+		}
+
+		public override void ProcessResult(QueryResult queryResult)
+		{
+			_query.ProcessResult(queryResult);
+			if (_query.Result.Count > 0)
+				_callback?.Invoke(_query.Result.First());
+		}
+
+		public override async Task ProcessResultAsync(QueryResult queryResult)
+		{
+			await _query.ProcessResultAsync(queryResult);
+			if (_query.Result.Count > 0)
+				_callback?.Invoke(_query.Result.First());
+		}
+	}
+
+	public class WithFirstTupleResultQuery<T1, T2> : Query
+		where T1 : class
+		where T2 : class
+	{
+		private readonly QueryWithTupleResult<T1, T2> _query;
+		private readonly Action<(T1, T2)> _callback;
+
+		public WithFirstTupleResultQuery(QueryWithTupleResult<T1, T2> query, Action<(T1, T2)> callback)
+			: base(query.QueryExpression, query.ProducesResultSet)
+		{
+			_query = query;
+			_callback = callback;
+		}
+
+		public override void ProcessResult(QueryResult queryResult)
+		{
+			_query.ProcessResult(queryResult);
+			if (_query.Result.Count > 0)
+				_callback?.Invoke(_query.Result.First());
+		}
+
+		public override async Task ProcessResultAsync(QueryResult queryResult)
+		{
+			await _query.ProcessResultAsync(queryResult);
+			if (_query.Result.Count > 0)
+				_callback?.Invoke(_query.Result.First());
 		}
 	}
 }
---
+++ Silk.Data.SQL.ORM/QueryExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Silk.Data.SQL.ORM.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silk.Data.SQL.ORM
+{
+	public static class QueryExtensions
+	{
+		public static Query WithResult<T>(this QueryWithMappedResult<T> query, Action<ICollection<T>> callback)
+			where T : class
+		{
+			return new WithMappedResultQuery<T>(query, callback);
+		}
+
+		public static Query WithResult<T>(this QueryWithScalarResult<T> query, Action<ICollection<T>> callback)
+			where T : struct
+		{
+			return new WithScalarResultQuery<T>(query, callback);
+		}
+
+		public static Query WithResult<T1, T2>(this QueryWithTupleResult<T1, T2> query, Action<ICollection<(T1, T2)>> callback)
+			where T1 : class
+			where T2 : class
+		{
+			return new WithTupleResultQuery<T1, T2>(query, callback);
+		}
+
+		public static Query WithFirstResult<T>(this QueryWithMappedResult<T> query, Action<T> callback)
+			where T : class
+		{
+			return new WithFirstMappedResultQuery<T>(query, callback);
+		}
+
+		public static Query WithFirstResult<T>(this QueryWithScalarResult<T> query, Action<T> callback)
+			where T : struct
+		{
+			return new WithFirstScalarResultQuery<T>(query, callback);
+		}
+
+		public static Query WithFirstResult<T1, T2>(this QueryWithTupleResult<T1, T2> query, Action<(T1, T2)> callback)
+			where T1 : class
+			where T2 : class
+		{
+			return new WithFirstTupleResultQuery<T1, T2>(query, callback);
+		}
+	}
+}
