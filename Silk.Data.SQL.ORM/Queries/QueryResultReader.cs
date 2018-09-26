adf7354137b92196c87d8a11f5fb0c4feef088ab
John Carruthers <johnc@frag-labs.com>
2018-09-25 20:35:54 -0500

    Add result reader methods for nullable types.
    Fixes #4.

--- Silk.Data.SQL.ORM/Queries/QueryResultReader.cs
+++ Silk.Data.SQL.ORM/Queries/QueryResultReader.cs
@@ -20,7 +20,18 @@ namespace Silk.Data.SQL.ORM.Queries
 				{ typeof(decimal), new Func<QueryResult, int, decimal>((q,o) => q.GetDecimal(o)) },
 				{ typeof(string), new Func<QueryResult, int, string>((q,o) => q.GetString(o)) },
 				{ typeof(Guid), new Func<QueryResult, int, Guid>((q,o) => q.GetGuid(o)) },
-				{ typeof(DateTime), new Func<QueryResult, int, DateTime>((q,o) => q.GetDateTime(o)) }
+				{ typeof(DateTime), new Func<QueryResult, int, DateTime>((q,o) => q.GetDateTime(o)) },
+
+				{ typeof(bool?), new Func<QueryResult, int, bool?>((q,o) => q.GetBoolean(o)) },
+				{ typeof(byte?), new Func<QueryResult, int, byte?>((q,o) => q.GetByte(o)) },
+				{ typeof(short?), new Func<QueryResult, int, short?>((q,o) => q.GetInt16(o)) },
+				{ typeof(int?), new Func<QueryResult, int, int?>((q,o) => q.GetInt32(o)) },
+				{ typeof(long?), new Func<QueryResult, int, long?>((q,o) => q.GetInt64(o)) },
+				{ typeof(float?), new Func<QueryResult, int, float?>((q,o) => q.GetFloat(o)) },
+				{ typeof(double?), new Func<QueryResult, int, double?>((q,o) => q.GetDouble(o)) },
+				{ typeof(decimal?), new Func<QueryResult, int, decimal?>((q,o) => q.GetDecimal(o)) },
+				{ typeof(Guid?), new Func<QueryResult, int, Guid?>((q,o) => q.GetGuid(o)) },
+				{ typeof(DateTime?), new Func<QueryResult, int, DateTime?>((q,o) => q.GetDateTime(o)) }
 			};
 
 		public QueryResult QueryResult { get; }
