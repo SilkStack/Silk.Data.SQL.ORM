cbaf7b21fc7e66c1b8ed9883067113b2582129a7
John Carruthers <johnc@frag-labs.com>
2018-11-15 00:14:39 -0600

    Add more Set APIs for EntityInsertBuilder to support more scenarios like UpdateBuilder already does, allow fields to be specified out of order.

--- Silk.Data.SQL.ORM/Queries/InsertBuilder.cs
+++ Silk.Data.SQL.ORM/Queries/InsertBuilder.cs
@@ -4,6 +4,7 @@ using Silk.Data.SQL.ORM.Schema;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Silk.Data.SQL.ORM.Queries
 {
@@ -11,20 +12,20 @@ namespace Silk.Data.SQL.ORM.Queries
 		where T : class
 	{
 		private readonly List<string> _columnNames = new List<string>();
-		private readonly List<List<QueryExpression>> _rowValues = new List<List<QueryExpression>>();
-		private List<QueryExpression> _currentValueExpressions;
+		private readonly List<Dictionary<string,QueryExpression>> _rowValues = new List<Dictionary<string,QueryExpression>>();
+		private Dictionary<string, QueryExpression> _currentValueExpressions;
 
 		public EntityInsertBuilder(Schema.Schema schema, ObjectReadWriter entityReadWriter = null)
 			: base(schema, entityReadWriter)
 		{
-			_currentValueExpressions = new List<QueryExpression>();
+			_currentValueExpressions = new Dictionary<string, QueryExpression>();
 			_rowValues.Add(_currentValueExpressions);
 		}
 
 		public EntityInsertBuilder(EntitySchema<T> schema, ObjectReadWriter entityReadWriter = null)
 			: base(schema, entityReadWriter)
 		{
-			_currentValueExpressions = new List<QueryExpression>();
+			_currentValueExpressions = new Dictionary<string, QueryExpression>();
 			_rowValues.Add(_currentValueExpressions);
 		}
 
@@ -42,7 +43,7 @@ namespace Silk.Data.SQL.ORM.Queries
 				if (_currentValueExpressions.Count != _columnNames.Count)
 					throw new InvalidOperationException("Column cound and value count mismatch.");
 
-				_currentValueExpressions = new List<QueryExpression>();
+				_currentValueExpressions = new Dictionary<string, QueryExpression>();
 				_rowValues.Add(_currentValueExpressions);
 			}
 		}
@@ -51,24 +52,58 @@ namespace Silk.Data.SQL.ORM.Queries
 		{
 			AddColumnName(schemaField.Column);
 			var valueExpression = schemaField.ExpressionFactory.Value(entity, EntityReadWriter);
-			_currentValueExpressions.Add(valueExpression);
+			_currentValueExpressions[schemaField.Column.ColumnName] = valueExpression;
 		}
 
 		public void Set<TValue>(ISchemaField<T> schemaField, TValue value)
 		{
 			AddColumnName(schemaField.Column);
 			var valueExpression = QueryExpression.Value(value);
-			_currentValueExpressions.Add(valueExpression);
+			_currentValueExpressions[schemaField.Column.ColumnName] = valueExpression;
+		}
+
+		public void Set<TProperty>(Expression<Func<T, TProperty>> fieldSelector, TProperty value)
+		{
+			var columnExpression = ExpressionConverter.Convert(fieldSelector)
+				.QueryExpression as ColumnExpression;
+			if (columnExpression == null)
+				throw new InvalidOperationException("Invalid property selector expression.");
+			var valueExpression = QueryExpression.Value(value);
+			_currentValueExpressions[columnExpression.ColumnName] = valueExpression;
+		}
+
+		public void Set<TProperty>(Expression<Func<T, TProperty>> fieldSelector, Expression<Func<T, TProperty>> valueExpression)
+		{
+			var columnExpression = ExpressionConverter.Convert(fieldSelector)
+				.QueryExpression as ColumnExpression;
+			if (columnExpression == null)
+				throw new InvalidOperationException("Invalid property selector expression.");
+			var valueExpressionResult = ExpressionConverter.Convert(valueExpression);
+			_currentValueExpressions[columnExpression.ColumnName] = valueExpressionResult.QueryExpression;
 		}
 
 		public override QueryExpression BuildQuery()
 		{
-			var valuesArray = _rowValues.Select(row => row.Select(value => value).ToArray()).ToArray();
 			return QueryExpression.Insert(
 				Source.TableName,
 				_columnNames.ToArray(),
-				valuesArray
+				GetRows()
 				);
+		}
+
+		private QueryExpression[][] GetRows()
+		{
+			var nameCount = _columnNames.Count;
+			var rows = new QueryExpression[_rowValues.Count][];
+			for (var i = 0; i < rows.Length; i++)
+			{
+				rows[i] = new QueryExpression[nameCount];
+				for (var j = 0; j < nameCount; j++)
+				{
+					rows[i][j] = _rowValues[i][_columnNames[j]];
+				}
+			}
+			return rows;
 		}
 	}
 
